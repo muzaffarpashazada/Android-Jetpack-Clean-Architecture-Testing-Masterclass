7286e728cc6a81dffa88475d7432fe4cfcbadfaa
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-15 03:25:27 +0400

    [Section 6] : [Video 32] - Add bundle for sent data

--- NavDemo1/app/src/main/java/com/anushka/navdemo1/HomeFragment.kt
+++ NavDemo1/app/src/main/java/com/anushka/navdemo1/HomeFragment.kt
@@ -1,9 +1,12 @@
 package com.anushka.navdemo1
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -19,7 +22,12 @@ class HomeFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 
         binding.button.setOnClickListener {
-            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+            if (TextUtils.isEmpty(binding.editTextView.text.toString())) {
+                Toast.makeText(context, "Please write your name", Toast.LENGTH_LONG).show()
+            } else {
+                val bundle: Bundle = bundleOf("user_input" to binding.editTextView.text.toString())
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
+            }
         }
         return binding.root
     }
