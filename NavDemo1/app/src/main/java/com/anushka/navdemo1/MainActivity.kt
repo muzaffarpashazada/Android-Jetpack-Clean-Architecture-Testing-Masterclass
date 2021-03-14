201937be079eca6b5ce792cdfad1f15723109720
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-15 02:29:35 +0400

    implement navigation and data binding

--- NavDemo1/app/src/main/java/com/anushka/navdemo1/MainActivity.kt
+++ NavDemo1/app/src/main/java/com/anushka/navdemo1/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.databinding.DataBindingUtil
 import com.anushka.navdemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
