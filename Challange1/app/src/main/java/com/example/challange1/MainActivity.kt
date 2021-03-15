ceda9f4f86ce5f9014ab43ee6bb20d6892583d2d
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-16 03:41:34 +0400

    [Section 8] : [Video 42] - Add Main and IO Thread for log

--- Challange1/app/src/main/java/com/example/challange1/MainActivity.kt
+++ Challange1/app/src/main/java/com/example/challange1/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.challange1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            for (i in 1..20000) {
+                Log.e("Muzafferus", "this is ${Thread.currentThread().name} -  $i")
+            }
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            for (i in 1..20000) {
+                Log.e("Muzafferus", "this is  ${Thread.currentThread().name} -  $i")
+            }
+        }
     }
 }
