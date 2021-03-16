e0bf159b4118e08523cfd86b7b0451772144a9a9
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-16 10:24:56 +0400

    [Section 8] : [Video 48] - first suspend function test

--- AsyncAwaitDemo/app/src/main/java/com/anushka/asyncawaitdemo/MainActivity.kt
+++ AsyncAwaitDemo/app/src/main/java/com/anushka/asyncawaitdemo/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.anushka.asyncawaitdemo
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -15,7 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        CoroutineScope(IO).launch {
+            Log.e("Muzafferus", "Calculation prosses started...")
+            val firstStock = getFirstStock()
+            val secondStock = getSecondStock()
+            val total = firstStock + secondStock
+            Log.e("Muzafferus", "Total is $total")
+        }
 
     }
+}
+
+private suspend fun getFirstStock(): Int {
+    delay(10000)
+    Log.e("Muzafferus", "first stock returned")
+    return 55000
+}
+
+private suspend fun getSecondStock(): Int {
+    delay(8000)
+    Log.e("Muzafferus", "second stock returned")
+    return 35000
 }
