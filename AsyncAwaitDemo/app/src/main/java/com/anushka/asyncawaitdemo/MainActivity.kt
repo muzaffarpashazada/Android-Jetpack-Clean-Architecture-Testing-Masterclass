3c76293d7c7e8d2d99126564594ecf7ef6a5b1cd
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-16 10:27:40 +0400

    [Section 8] : [Video 48] - add async

--- AsyncAwaitDemo/app/src/main/java/com/anushka/asyncawaitdemo/MainActivity.kt
+++ AsyncAwaitDemo/app/src/main/java/com/anushka/asyncawaitdemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -17,9 +18,9 @@ class MainActivity : AppCompatActivity() {
 
         CoroutineScope(IO).launch {
             Log.e("Muzafferus", "Calculation prosses started...")
-            val firstStock = getFirstStock()
-            val secondStock = getSecondStock()
-            val total = firstStock + secondStock
+            val firstStock = async { getFirstStock() }
+            val secondStock = async { getSecondStock() }
+            val total = firstStock.await() + secondStock.await()
             Log.e("Muzafferus", "Total is $total")
         }
 
