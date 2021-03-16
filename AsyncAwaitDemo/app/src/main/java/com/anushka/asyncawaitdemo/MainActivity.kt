fa355e02d1892165588c1af9a00c2da85905be32
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-16 09:39:05 +0400

    [Section 8] : [Video 48] - init new project

---
+++ AsyncAwaitDemo/app/src/main/java/com/anushka/asyncawaitdemo/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.anushka.asyncawaitdemo
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
