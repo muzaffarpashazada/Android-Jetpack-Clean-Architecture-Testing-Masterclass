dc1565471deeca5af9de92d6dcd343127ac32a57
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-26 01:04:08 +0400

    [Section 9] : [Video 63] - Create RecyclerViewAdapter - Show data

--- RoomDemo/app/src/main/java/com/muzafferus/roomdemo/MainActivity.kt
+++ RoomDemo/app/src/main/java/com/muzafferus/roomdemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.muzafferus.roomdemo.databinding.ActivityMainBinding
 import com.muzafferus.roomdemo.db.SubscriberDatabase
 import com.muzafferus.roomdemo.db.SubscriberRepository
@@ -18,25 +19,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        viewModel = ViewModelProvider(
-            this,
-            SubscriberViewModelFactory(
-                SubscriberRepository(
-                    SubscriberDatabase.getInstance(application).subscriberDao
-                )
-            )
-        ).get(SubscriberViewModel::class.java)
-
+        val dao = SubscriberDatabase.getInstance(application).subscriberDao
+        val repository = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, factory).get(SubscriberViewModel::class.java)
         binding.myViewModel = viewModel
         binding.lifecycleOwner = this
-
-        displaySubscriberList()
+        initRecyclerView()
     }
 
-    private fun displaySubscriberList() {
+    private fun initRecyclerView() {
+        binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
+        displaySubscribersList()
+    }
+
+    private fun displaySubscribersList() {
         viewModel.subscribers.observe(this, Observer {
-            Log.e("YER", "it: ${it.toString()}")
+            binding.subscriberRecyclerView.adapter = MyRecyclerViewAdapter(it)
         })
     }
 }
