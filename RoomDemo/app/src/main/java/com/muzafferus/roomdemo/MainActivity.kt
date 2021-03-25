156c4330ab28080edfef3138854c4364eab6af7b
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-26 00:13:59 +0400

    [Section 9] : [Video 61] - Create ViewModel and Factory - Connect binding and functions (save and delete)

--- RoomDemo/app/src/main/java/com/muzafferus/roomdemo/MainActivity.kt
+++ RoomDemo/app/src/main/java/com/muzafferus/roomdemo/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.muzafferus.roomdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.muzafferus.roomdemo.databinding.ActivityMainBinding
+import com.muzafferus.roomdemo.db.SubscriberDatabase
+import com.muzafferus.roomdemo.db.SubscriberRepository
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SubscriberViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        viewModel = ViewModelProvider(
+            this,
+            SubscriberViewModelFactory(
+                SubscriberRepository(
+                    SubscriberDatabase.getInstance(application).subscriberDao
+                )
+            )
+        ).get(SubscriberViewModel::class.java)
+
+        binding.myViewModel = viewModel
+        binding.lifecycleOwner = this
+
+        displaySubscriberList()
+    }
+
+    private fun displaySubscriberList() {
+        viewModel.subscribers.observe(this, Observer {
+            Log.e("YER", "it: ${it.toString()}")
+        })
     }
 }
---
+++ RoomDemo/app/src/main/java/com/muzafferus/roomdemo/SubscriberViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.muzafferus.roomdemo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.muzafferus.roomdemo.db.SubscriberRepository
+
+class SubscriberViewModelFactory(private val repository: SubscriberRepository) :
+    ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(SubscriberViewModel::class.java)) {
+            return SubscriberViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unkown View Model class")
+    }
+}
