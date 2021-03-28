535782dddeac8e73194bbd80755cd500696ed4dc
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-29 03:06:15 +0400

    [Section 11] : [Video 80] - Create simple Demo Notification

--- NotificationDemo/app/src/main/java/com/muzafferus/notificationdemo/MainActivity.kt
+++ NotificationDemo/app/src/main/java/com/muzafferus/notificationdemo/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.muzafferus.notificationdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val channelId = "com.muzafferus.notificationdemo.channel1"
+    private var notificationManager: NotificationManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel(channelId, "DemoChannel", "this is a demo notification channel")
+
+
+        button.setOnClickListener {
+            displayNotification()
+        }
+    }
+
+    private fun displayNotification() {
+        val notificationId = 45
+        val notification = NotificationCompat.Builder(this@MainActivity, channelId)
+                .setContentTitle("Demo Title")
+                .setContentText("This is demo notification")
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build()
+        notificationManager?.notify(notificationId, notification)
+    }
+
+    private fun createNotificationChannel(id: String, name: String, channelDescription: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = channelDescription
+            }
+            notificationManager?.createNotificationChannel(channel)
+        }
     }
 }
