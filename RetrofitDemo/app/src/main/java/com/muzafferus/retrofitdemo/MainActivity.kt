14678bb67d3953ded73c92cb26f2bdfa57c89fd3
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-03-29 01:53:05 +0400

    [Section 10] : [Video 74] - Add retrofit request get data and set view

--- RetrofitDemo/app/src/main/java/com/muzafferus/retrofitdemo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/muzafferus/retrofitdemo/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.muzafferus.retrofitdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofitService: AlbumService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retrofitService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumList = it.body()?.listIterator()
+            if (albumList != null) {
+                while (albumList.hasNext()) {
+                    val albumListItem = albumList.next()
+                    val result = " " + "Album Title :  ${albumListItem.title}" + "\n" +
+                            " " + "Album id : ${albumListItem.id}" + "\n" +
+                            " " + "Album userId : ${albumListItem.userId}" + "\n\n\n"
+
+                    textView.append(result)
+                }
+            }
+        })
     }
 }
