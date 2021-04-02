1f1b6c5ef40363eb5ef533c6d96730284f50d76c
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-04-02 04:37:25 +0400

    [Section 11] : [Video 87] Set Constraints

--- WorkMagaerDemo1/app/src/main/java/com/muzafferus/workmagaerdemo1/MainActivity.kt
+++ WorkMagaerDemo1/app/src/main/java/com/muzafferus/workmagaerdemo1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.muzafferus.workmagaerdemo1
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,8 +21,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun setOnTimeWorkRequest() {
         val workManager = WorkManager.getInstance(applicationContext)
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
 
-        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraints)
+            .build()
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
             .observe(this, Observer {
