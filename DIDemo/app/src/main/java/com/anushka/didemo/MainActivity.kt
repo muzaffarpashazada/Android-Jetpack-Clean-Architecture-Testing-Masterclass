697381a62ee5ee39f82eec50e8b5b5f3b3035431
muzaffar.pashazada@ibar.az <3UsDcP%0HvI&Uiel>
2021-04-06 02:07:41 +0400

    [Section 13] : [Video 93] Constructor Injection With Dagger 2

--- DIDemo/app/src/main/java/com/anushka/didemo/MainActivity.kt
+++ DIDemo/app/src/main/java/com/anushka/didemo/MainActivity.kt
@@ -2,19 +2,27 @@ package com.anushka.didemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var smartPhone: SmartPhone
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val battery = Battery()
-        val memoryCard = MemoryCard()
-        val serviceProvider = ServiceProvider()
-        val simCard = SIMCard(serviceProvider)
+        DaggerSmartPhoneComponent
+            .create()
+            .getSmartPhone()
+            .makeACallWithRecording()
 
-        val smartPhone = SmartPhone(battery, simCard, memoryCard)
-        smartPhone.makeACallWithRecording()
+//        val battery = Battery()
+//        val memoryCard = MemoryCard()
+//        val serviceProvider = ServiceProvider()
+//        val simCard = SIMCard(serviceProvider)
+//
+//        val smartPhone = SmartPhone(battery, simCard, memoryCard)
+//        smartPhone.makeACallWithRecording()
     }
 }
