3c730a207fefa06559a687a85af10803941a2941
muzaffarpashazada <muzaffar.pashazada@ibar.az>
2021-07-02 23:10:52 +0400

    [Section 18] : [Video 140] - Fragments & Navigation Components

--- NewsAPICliend/app/src/main/java/com/muzafferus/newsapicliend/MainActivity.kt
+++ NewsAPICliend/app/src/main/java/com/muzafferus/newsapicliend/MainActivity.kt
@@ -2,10 +2,20 @@ package com.muzafferus.newsapicliend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import com.muzafferus.newsapicliend.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bnvNews.setupWithNavController(
+            fragment.findNavController()
+        )
     }
 }
